8ff7dee27f0000ca1790d289bf8b4cac5353ea20
Ivy Nguyen <ivynguyen003@gmail.com>
2018-08-26 16:19:21 -0700

    add unique post and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,22 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by!(
+  title:"Test Title 1",
+  body:"Test Body 1"
+)
+post1 = Post.all
+puts "#{Post.count}"
+
+
+puts "#{Comment.count}"
+Comment.find_or_create_by!(
+  post: post1,
+  body:"Test Body Unique 2"
+)
+puts "#{Comment.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
