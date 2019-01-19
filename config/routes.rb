65fa12157660545d04a878800497937eaa435b0b
Ivy Nguyen <ivynguyen003@gmail.com>
2019-01-19 14:30:18 -0800

    add bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :posts
   resources :topics do 
 
-    resources :posts, except: [:index]
+  resources :posts, except: [:index]
   end
 
+  resources :users, only: [:new, :create]
   get 'about' => 'welcome#about'
 
   get 'faq' => 'welcome#faq'
