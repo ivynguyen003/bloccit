01ea1fc154a59546219565f4adabe315082d29ee
Ivy Nguyen <ivynguyen003@gmail.com>
2019-01-19 20:38:30 -0800

    add signup link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+      @user = User.new
+      @user.name = params[:user][:name]
+      @user.email = params[:user][:email]
+      @user.password = params[:user][:password]
+      @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
+    end
+  end
 end
