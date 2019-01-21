cf9a00ba1def3c1fb9240a4011f11802bd83b6bc
Ivy Nguyen <ivynguyen003@gmail.com>
2019-01-20 20:39:56 -0800

    confirmation-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+      @user = User.new
+      @user.name = params[:user][:name]
+      @user.email = params[:user][:email]
+      @user.password = params[:user][:password]
+      @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
+  def user_params
+   params.require(:user).permit(:email, :password, :password_confirmation)
+   helper_method :user_params
+  end
+
   def create
       @user = User.new
       @user.name = params[:user][:name]
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
   resources :topics do 
-
   resources :posts, except: [:index]
   end
 
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
 
   get 'faq' => 'welcome#faq'
